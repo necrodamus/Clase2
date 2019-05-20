2f228d7baa98a38cf66e63ffe4f49bc1b85a7936
ISABELLA VALENZI Andres O. TATA <aisabella@suppliers.tenaris.com>
2019-05-20 20:00:33 -0300

    Clase2

---
+++ app/src/main/java/andres/curso/clase2/MainActivity.java
@@ -0,0 +1,62 @@
+package andres.curso.clase2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // La actividad está a punto de Crearse.
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        //Notas
+        int Matematica = 6;
+        int Fisica = 7;
+        int Ingles = 5;
+        int Lengua  = 9;
+
+        int promedio = ( Matematica + Fisica + Ingles + Lengua ) / 4;
+
+        if ( promedio > 7) {
+            Toast.makeText(this, "Aprobo La Cursada", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Desaprobo la Cursada", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+    /*
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de hacerse visible.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        // La actividad se ha vuelto visible (ahora se "reanuda").
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        // Enfocarse en otra actividad  (esta actividad está a punto de ser "detenida").
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        // La actividad ya no es visible (ahora está "detenida")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        // La actividad está a punto de ser destruida.
+    }
+*/
+}
